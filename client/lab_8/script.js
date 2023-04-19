6271b4f7122aacb014ce2db0ff297cbdad59a83b
Erick Salmeron <ericksalmeron00@gmail.com>
2023-04-18 23:46:15 -0400

    mid commit

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -39,6 +39,29 @@ function getRandomIntInclusive(min, max) {
     });
   }
   
+  function initMap(){
+    const carto = l.map('map').setview([38.98, -76.93], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(carto);
+  }
+
+  function markerPlace(array, map) {
+    console.log('array for markers', array)
+    map.eachLayer((layer) => {
+      if (layer instanceof L.Marker) {
+        layer.remove();
+      }
+    });
+  
+    array.forEach((item) => {
+      console.log('markerPlace', item);
+      const { coordinates } = item.geocoded_column_1
+      L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    })
+  }
+
   async function mainEvent() {
     // the async keyword means we can make APv bfxbI requests
     const mainForm = document.querySelector(".main_form"); // This class name needs to be set on your form before you can listen for an event on it
@@ -50,8 +73,15 @@ function getRandomIntInclusive(min, max) {
     const loadAnimation = document.querySelector("#data_load_animation");
     loadAnimation.style.display = "none";
     generateListButton.classList.add("hidden");
+
+    const carto = initMap();
+
+    const storedData = localStorage.getItem('storedData');
+    let parsedData = JSON.parse(storedData);
+    if (parsedData?.length > 0){
+        generateListButton.classList.remove("hidden");
+    }
   
-    let storedList = [];
     let currentList = []; // this is "scoped" to the main event function
   
     /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
@@ -61,38 +91,25 @@ function getRandomIntInclusive(min, max) {
       loadAnimation.style.display = "inline-block";
   
       // Basic GET request - this replaces the form Action
-      const results = await fetch(
-        "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"
-      );
+      const results = await fetch("https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json");
   
       // This changes the response from the GET into data we can use - an "object"
-      storedList = await results.json();
-      if (storedList.length > 0){
-          generateListButton.classList.remove('hidden');
+      const storedList = await results.json();
+      localStorage.setItem('storedData', JSON.stringify(storedList));
+      parsedData = storedList
+
+      if (parsedData?.length >0){
+        generateListButton.classList.remove('hidden'):
       }
-  
       loadAnimation.style.display = "none";
-      console.table(storedList);
-    });
-  
-    filterButton.addEventListener("click", (event) => {
-      console.log("clicked FilterButton");
-  
-      const formData = new FormData(mainForm);
-      const formProps = Object.fromEntries(formData);
-  
-      console.log(formProps);
-      const newList = filterList(currentList, formProps.resto);
-  
-      console.log(newList);
-      injectHTML(newList);
     });
   
     generateListButton.addEventListener("click", (event) => {
       console.log("generate new list");
-      currentList = processRestaurants(storedList);
+      currentList = processRestaurants(parsedData);
       console.log(currentList);
       injectHTML(currentList);
+      markerPlace(currentList, carto)
     });
   
     textField.addEventListener("input", (event) => {
@@ -100,6 +117,13 @@ function getRandomIntInclusive(min, max) {
       const newList = filterList(currentList, event.target.value);
       console.log(newList);
       injectHTML(newList);
+      markerPlace(newList, carto);
+    });
+
+    clearDataButton.addEventListener("click", (event) => {
+        console.log('clear browser data');
+        localStorage.clear
+        console.log('localStorage Check', localStorage.getItem("storedData"))
     });
   }
   
